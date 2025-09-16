e1a0eff10922d252ad2f3a87f6dc62b1aba2c0cf
Gustavo Gines <ginesparker95@gmail.com>
2025-09-16 16:48:28 -0300

    chore: testing server

--- start-server.sh
+++ start-server.sh
@@ -4,13 +4,12 @@ php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
-set -e
-
 if [ -z "$APP_KEY" ]; then php artisan key:generate --force; fi
+
 php artisan config:cache
-php artisan route:cache
+# Omitir php artisan route:cache
 php artisan view:cache || true
 php artisan storage:link || true
-# Si usás DB:
-# php artisan migrate --force
+
 apache2-foreground
+
