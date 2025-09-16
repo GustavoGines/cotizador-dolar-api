4c22189caf821266bdc1d1d0b652490bade5dfe5
Gustavo Gines <ginesparker95@gmail.com>
2025-09-16 16:56:23 -0300

    chore: testing server

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthController;
 
+Route::get('/diag', function () {
+  return [
+    'ok' => true,
+    'routes_prefijo' => 'api', // recordatorio
+    'session' => config('session.driver'),
+    'db' => config('database.default'),
+  ];
+});
+
 Route::get('/', fn() => 'cotizador-dolar-api running ✅');
 
 Route::get('/', function () {
