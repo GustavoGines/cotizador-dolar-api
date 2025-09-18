b2400147f532b60dbac7c6d01d96f4085db7ed9d
Gustavo Gines <ginesparker95@gmail.com>
2025-09-18 14:24:19 -0300

    fix(docker): ajustar COPY para evitar error con tailwind/postcss faltantes. Incluye mejoras en seeders, database config, service y start-server para despliegue en Render.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    emptyOutDir: true,
+  },
 });
