ab9d77cad24bad9fbf6fc67472922c7f5e5d9505
Gustavo Gines <ginesparker95@gmail.com>
2025-09-18 20:48:54 -0300

    Actualizacion de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-    emptyOutDir: true,
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        outDir: 'public/build',   // 👈 asegura que se genere en /public/build
+        manifest: true,           // 👈 obligatorio para Laravel en prod
+        emptyOutDir: true,
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+    },
 });
