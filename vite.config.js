8dcf6bf3aeafac254a0cb0205baee15b0a96d7dc
Gustavo Gines <ginesparker95@gmail.com>
2025-09-19 13:23:40 -0300

    Arreglo de vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'public/build',   // 👈 asegura que se genere en /public/build
-        manifest: true,           // 👈 obligatorio para Laravel en prod
-        emptyOutDir: true,
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(), // 👈 activa Tailwind 4 en Vite
+  ],
+  build: {
+    outDir: 'public/build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: ['resources/css/app.css', 'resources/js/app.js'],
     },
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-    },
-});
+  },
+})
