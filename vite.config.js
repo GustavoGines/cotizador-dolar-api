cec2a21bf234694f80a5558104370a5e345e5813
Gustavo Gines <ginesparker95@gmail.com>
2025-09-19 00:03:13 -0300

    fix: incluir public en build de Vite

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
         outDir: 'public/build',
         manifest: true,
         emptyOutDir: true,
-        manifestDir: '.'
     },
     server: {
         host: '0.0.0.0',
