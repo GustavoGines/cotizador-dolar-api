a23e49d4dfcde12d054f4d3cbecb85b5d121af64
Gustavo Gines <ginesparker95@gmail.com>
2025-09-20 16:57:38 -0300

    Configurando todo la version 1.0.6

---
+++ app/Http/Controllers/Api/VersionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VersionController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        return response()->json([
+            'version'     => config('app_version.latest'),
+            'min_version' => config('app_version.minimum'),
+            'url'         => config('app_version.url'),
+        ]);
+    }
+}
---
+++ config/app_version.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Última versión disponible de la app
+    'latest' => '1.2.0',
+
+    // Versión mínima soportada (si el usuario tiene menos, debe actualizar sí o sí)
+    'minimum' => '1.0.5',
+
+    // URL de descarga de la APK
+    'url' => env('APP_UPDATE_URL', 'https://tuservidor.com/apk/cotizador-1.2.0.apk'),
+];
