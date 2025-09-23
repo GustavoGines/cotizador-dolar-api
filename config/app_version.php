2e2b9d62542a4848412d07ed090bfc73228638e1
Gustavo Gines <ginesparker95@gmail.com>
2025-09-23 03:02:18 -0300

    Actualizacion de url del apk version nueva

--- config/app_version.php
+++ config/app_version.php
@@ -8,7 +8,7 @@ return [
     'minimum' => env('APP_MIN_VERSION', '1.0.5'),
 
     // URL del APK (podés dejarla vacía hasta subir la 1.0.6)
-    'url'    => env('APP_UPDATE_URL', 'https://github.com/GustavoGines/cotizador-dolar-api/releases/download/v1.0.6/app-release.apk'),
+    'url'    => env('APP_UPDATE_URL', 'https://firebasestorage.googleapis.com/v0/b/cotizador-dolar-api.firebasestorage.app/o/releases%2Fapp-release-1.0.6.apk?alt=media&token=390f65a7-f6d2-42f6-b75a-ab3e03a8bf79'),
 
     // (opcional) notas de la versión para mostrar en Flutter
     'notes'   => env('APP_UPDATE_NOTES', 'Testing new version...'),
